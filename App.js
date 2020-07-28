e41a1b39d77bc0ec8d1a0779ec944d62b1eb6f67
Juan C Hernandez <37321973+JChern83@users.noreply.github.com>
2020-07-27 17:07:29 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom'
+import history from './components/history'
+import PrivateRoute from './components/PrivateRoute'
+import './App.css';
+import FoodTruckForm from './components/Operator/FoodTruckForm';
+import DinerTruckMenu from './components/User/DinerTruckMenu'
+import OperatorDashboard from './components/Operator/OperatorDashboard'
+import FoodTruckMenu from './components/Operator/FoodTruckMenu'
+import SignupPage from './components/login/SignupPage'
+import LoginPage from './components/login/LoginPage'
+import TestDiner from './components/User/testdiner';
+import TruckList from './components/User/TruckList';
+
+function App() {
+  return (
+
+    <>
+      <Router history={history}>
+        <Switch>
+          <PrivateRoute exact path='/diner/dashboard' component={TestDiner} />
+          <PrivateRoute exact path='/diner/trucks' component={TruckList} />
+          <PrivateRoute exact path='/diner/trucks/:id' component={DinerTruckMenu} />
+          <PrivateRoute exact path='/operator/dashboard' component={OperatorDashboard} />
+          <PrivateRoute exact path='/operator/add-trucks' component={FoodTruckForm} />
+          <PrivateRoute exact path='/operator/:id' component={FoodTruckMenu} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/register' component={SignupPage} />
+          <Route component={LoginPage} />
+        </Switch>
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
